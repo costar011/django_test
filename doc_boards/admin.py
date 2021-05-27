4bf488b5f80c540eb95a79a8ee2ead08c9e570c5
costar011 <dnaer456@gmail.com>
2021-05-27 22:28:18 +0900

    screen error

--- doc_boards/admin.py
+++ doc_boards/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.DocBoardsModel)
+class DocBoardAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "title",
+        "displayUser",
+        "board_type",
+        "created",
+        "likeCount",
+    )
+    raw_id_fields = ("author",)
--- doc_boards/models.py
+++ doc_boards/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class DocBoardsModel(models.Model):
+
+    CHOICE_HTML = "html"
+    CHOICE_CSS = "css"
+    CHOICE_JS = "javascript"
+    CHOICE_ES6 = "eS6"
+    CHOICE_NODE = "node.js"
+    CHOICE_JAVA = "java"
+    CHOICE_PYTHON = "python"
+    CHOICE_SQL = "sql"
+    CHOICE_REACT = "react"
+    CHOICE_RN = "react-native"
+    CHOICE_FIREBASE = "firebase"
+    CHOICE_GRAPHQL = "graphql"
+
+    CHOICE_TYPE = (
+        (CHOICE_HTML, "HTML"),
+        (CHOICE_CSS, "CSS"),
+        (CHOICE_JS, "JAVASCRIPT"),
+        (CHOICE_ES6, "ES6"),
+        (CHOICE_NODE, "NODE"),
+        (CHOICE_JAVA, "JAVA"),
+        (CHOICE_PYTHON, "PYTHON"),
+        (CHOICE_SQL, "SQL"),
+        (CHOICE_REACT, "REACT"),
+        (CHOICE_RN, "REACT-NATIVE"),
+        (CHOICE_FIREBASE, "FIREBASE"),
+        (CHOICE_GRAPHQL, "GRAPHQL"),
+    )
+
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, related_name="doc_board", on_delete=models.CASCADE)
+    board_type = models.CharField(choices=CHOICE_TYPE, max_length=40)
+
+    description = models.TextField()
+    likeCount = models.IntegerField(default=0)
+
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    def displayUser(self):
+        return self.author.username
+
+    displayUser.short_description = "author"
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "DOC_BOARDS"
