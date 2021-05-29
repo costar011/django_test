0b91e7968254fd554b58b01353f7e46a70307834
costar011 <dnaer456@gmail.com>
2021-05-29 14:59:27 +0900

    doc_board add complete

--- doc_boards/admin.py
+++ doc_boards/admin.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.FileModel)
+class FileAdmin(admin.ModelAdmin):
+    raw_id_fields = ("board",)
+
+
+class FileInline(admin.TabularInline):
+    model = models.FileModel
+
+
 @admin.register(models.DocBoardsModel)
 class DocBoardAdmin(admin.ModelAdmin):
+
+    inlines = (FileInline,)
 
     list_display = (
         "title",
--- doc_boards/models.py
+++ doc_boards/models.py
@@ -2,6 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class FileModel(models.Model):
+
+    caption = models.CharField(max_length=100)
+    file = models.FileField()
+    board = models.ForeignKey(
+        "DocBoardsModel",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.caption
+
+    class Meta:
+        verbose_name_plural = "Files"
+
+
 class DocBoardsModel(models.Model):
 
     CHOICE_HTML = "html"
